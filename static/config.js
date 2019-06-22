3205b0676bfa35349dead9d9ccdf62555c294b0b
vhqd <www.zhangqiangwo@qq.com>
2019-06-22 18:19:19 +0800

    adjustment of project structure

--- static/config.js
+++ static/config.js
@@ -1,15 +1,20 @@
 var frontMode = "dev";
 var frontLocalUrl = "/" + window.location.pathname.split("/")[1];
-var pluginsUrl = "resources/plugins/";
-var staticUrl = "static/js/";
+var pluginsUrl = "../resources/plugins/";
+var staticUrl = "js/";
 
 var requireConfig = {
 	paths : {
 		"mui":staticUrl+"mui.min",
 		"zepto":staticUrl+"zepto.min",
-		"index":"static/index",
+		"index":"index",
 		"base":staticUrl+"base",
 		"listviews" : pluginsUrl+"bnc-list-default/bnc-list-default",
+		"bncalert" : pluginsUrl+"bnc-alert/bnc-alert",
+		"test":"test"
+	},
+	shim : {
+		
 	}
 }
 /**
@@ -19,7 +24,7 @@ require.config(requireConfig);
 /**
  * require 加载默认组件
  */
-var defaultLoad = ["listviews","mui","index","base","zepto"];
-require(defaultLoad, function(listviews,mui,index) {
+var defaultLoad = ["mui","index","base","zepto","test"];
+require(defaultLoad, function() {
 	console.log("默认加载了以下模块:" + JSON.stringify(defaultLoad));
 });
---
+++ static/test.js
@@ -0,0 +1,86 @@
+var index = 0;
+
+function compDialog() {
+	require(["listviews", "mui", "zepto", "base"], function(listviews, mui, $, base) {
+		/*	console.log(listviews.init({},1));
+			console.log('=======================================');
+			console.log(listviews.init({},2));
+			console.log('=======================================');
+			console.log(listviews.init({},3));*/
+		$("#list-box").empty();
+		$("#list-box").append(listviews.init({}, 1, index));
+		$("#list-box").append(listviews.init({}, 2, index));
+		mui("#list-box")[0].insertAdjacentHTML('beforeend', listviews.init({}, 3, index));
+		index++;
+		mui.ajax({
+			url: 'http://yapi.demo.qunar.com/mock/65528/api/login',
+			type: 'get',
+			dataType: 'json',
+			data: {},
+			async: true,
+			timeout: 10000,
+			cache: false,
+			success: function(data) {
+				console.log('测试ajax返回数据：');
+				console.log(JSON.stringify(data));
+				$('#ajax-box').empty().append(JSON.stringify(data))
+			},
+			error: function(data) {
+				mui.toast(data.msg);
+			}
+		});
+		/*base.ajax({
+				url:'http://yapi.demo.qunar.com/mock/65528/api/login',
+		    data:{ },
+		    method:'get',
+		    dataType:'json',
+		    async:true
+			},function(data){
+				console.log('执行成功了哦');
+				console.log(JSON.stringify(data));
+				$('#padded-box').append(JSON.stringify(data))
+			})*/
+	})
+}
+
+function goAlert() {
+	require(["mui"], function(mui) {
+		mui.openWindow({
+			url: "resources/plugins/bnc-alert/testAlert.html",
+			id: "s",
+			styles: {
+				popGesture: "close",
+				statusbar: {
+					background: "#f7f7f7"
+				}
+			},
+		});
+
+	})
+}
+require(["bncalert", "zepto"], function(bnc, $) {
+	$("body div").on('click', '.alert', function() {
+		var $this = $(this);
+		var type = $this.attr('data-flag');
+		console.log(type);
+		bnc({
+			content: '<h1 style="display:flex;justify-content:center;">我是弹框</h1>',
+			effect: type,
+			blankclose: true,
+			//autoClose: true
+		})
+
+	})
+})
+/*function a(){
+	require(["bncalert","zepto"],function(bnc,$){
+		console.log($);
+		console.log(bnc);
+		bnc({
+        content: '<h1 style="display:flex;justify-content:center;">我是弹框</h1>',
+        effect: "fadeIn",
+        blankclose: true,
+        //autoClose: true
+    })
+	})
+}*/
